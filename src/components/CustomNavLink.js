c092c0fcf38df0dded5c2f7ac143c33a0a8e97dc
Chaplin Artem <75485190+ArCheeq@users.noreply.github.com>
2023-04-11 15:56:21 +0300

    minor changes - adding styles

---
+++ src/components/CustomNavLink.js
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+function CustomNavLink({label, path}) {
+    return (
+        <NavLink className={({isActive}) => "font-bold uppercase " + (isActive ? "text-primary-200" : "")} to={path}>
+            {label}
+        </NavLink>
+    )
+}
+
+export default CustomNavLink;
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router-dom";
+import { useAuth } from "react-oidc-context";
+
+import CustomNavLink from "./CustomNavLink";
+
+function Header() {
+    const { isAuthenticated, signinRedirect } = useAuth();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        signinRedirect();
+    }
+
+  return (
+    <header className="bg-bg-100 mb-[60px]">
+      <div className="container px-6 py-5">
+        <nav className="flex justify-between items-center">
+          <CustomNavLink label="Логотип" path="/" />
+          <CustomNavLink label="Оголошення" path="/posts" />
+          <CustomNavLink label="Про нас" path="/aboutUs" />
+          {isAuthenticated ? (
+            <CustomNavLink label="Профіль" path="/userProfile" />
+          ) : (
+            <a
+              href="#"
+              onClick={handleClick}
+              className="font-bold uppercase"
+            >
+              Увійти
+            </a>
+          )}
+          <NavLink
+            className="max-w-[300px] w-full font-bold uppercase bg-primary-100 text-center py-4 rounded-xl"
+            to="/createPost"
+          >
+            Створити оголошення
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;

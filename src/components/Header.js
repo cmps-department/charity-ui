a8c2442e6f71175d3e94c528046d4eca0f4b5b74
Chaplin Artem <75485190+ArCheeq@users.noreply.github.com>
2023-04-26 13:27:45 +0300

    fix card and intro banner styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,22 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "react-oidc-context";
 import user from "../images/user.png";
 import logo from "../images/logo.png";
-import { Link } from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
 
 import CustomNavLink from "./CustomNavLink";
 
 function Header() {
-    const { isAuthenticated, signinRedirect } = useAuth();
+  const { isAuthenticated, signinRedirect } = useAuth();
 
-    function login() {
-        signinRedirect();
-    }
+  function login() {
+    signinRedirect();
+  }
 
   return (
     <header className="bg-bg-100">
       <div className="container max-w-xl px-4 py-5">
         <nav className="flex justify-between items-center">
-        <Link to="/">
+          <Link to="/">
             <img src={logo} width={240} height={230} alt="logo" />
           </Link>
           <CustomNavLink label="Оголошення" path="/" />
@@ -28,16 +26,12 @@ function Header() {
             <CustomNavLink label="Профіль" path="/userProfile" />
           ) : (
             <button
-            onClick={login}
-            className="font-bold uppercase flex items-center"
-          >
-            <img
-              alt="Профіль"
-              img src={user}
-              className="w-6 h-6 mr-2" 
-            />
-            Профіль
-          </button>
+              onClick={login}
+              className="font-bold uppercase flex items-center"
+            >
+              <img alt="Профіль" img src={user} className="w-6 h-6 mr-2" />
+              Профіль
+            </button>
           )}
           <NavLink
             className="max-w-[300px] w-full font-semibold uppercase bg-primary-100 text-center py-4 rounded-xl"
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,27 @@
 import heart from "../images/heart.png";
 import funnel from "../images/funnel.png";
 import descending from "../images/descending.png";
-import top from "../images/top.png";
-import done from "../images/done.png";
+import welfare from "../images/welfare.png";
+import verified from "../images/verified.png";
 import CardList from "../components/CardList";
 
 function HomePage() {
   return (
     <main className="container max-w-xl mx-auto my-15 px-4">
-      <h1 className="font-bold text-3xl flex items-center">
+      <h1 className="font-bold text-3xl flex items-center ml-[-60px]">
         <img src={heart} alt="Heart" className="inline-block mr-3" />
         Привіт, волонтере!
       </h1>
       <div className="flex justify-between gap-x-4 mt-8">
-        <div className="w-full bg-gradient-to-b from-orange-500 to-orange-300 rounded-xl flex border accent-200">
-          <img src={top} alt="top" className="h-full object-cover" />
+        <div className="w-full bg-gradient-to-b from-orange-500 to-orange-300 rounded-xl flex border accent-200 p-6">
+          <img src={welfare} alt="welfare"/>
           <div className="p-4 flex flex-col justify-center mx-auto">
             <h2 className="text-xl font-bold mb-2">Заголовок</h2>
             <p className="text-gray-700 text-base">Текст</p>
           </div>
         </div>
-        <div className="w-full bg-gradient-to-b from-green-500 to-green-300 rounded-xl flex border accent-200">
-          <img src={done} alt="Done" className="h-full object-cover" />
+        <div className="w-full bg-gradient-to-b from-green-500 to-green-300 rounded-xl flex border accent-200 p-6">
+          <img src={verified} alt="verified"/>
           <div className="p-4 flex flex-col justify-center mx-auto">
             <h2 className="text-xl font-bold mb-2">Заголовок</h2>
             <p className="text-gray-700 text-base">Текст</p>

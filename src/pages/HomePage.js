be5563580d8e130d2161d244e7edc4f8d98839e5
Chaplin Artem <75485190+ArCheeq@users.noreply.github.com>
2023-05-19 16:22:54 +0300

    routing update: added lazy component loading and preloader display

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,37 +9,45 @@ import Filters from "../components/Filters";
 
 function HomePage() {
   return (
-    <main className="container max-w-xl mx-auto my-15 px-4">
-      <Title imageUrl={heart} alt="heart" title="Привіт, волонтере!"/>
+    <>
+      <Title imageUrl={heart} alt="heart" title="Привіт, волонтере!" />
       <div className="flex justify-between gap-x-4 mb-10">
-        <div className="w-full bg-gradient-to-br from-or-300 from-0% to-or-500
-                       to-100% rounded-3xl flex gap-12 border border-accent-100 p-6">
+        <div
+          className="w-full bg-gradient-to-br from-or-300 from-0% to-or-500
+                       to-100% rounded-3xl flex gap-12 border border-accent-100 p-6"
+        >
           <div className="w-6/12 flex flex-col justify-center align-center">
             <img width={274} height={274} src={welfare} alt="welfare" />
           </div>
           <div className="w-6/12 p-4 flex flex-col justify-center mx-auto">
             <h2 className="text-xl font-bold mb-4">Допомогати - круто</h2>
             <p className="text-gray-700 text-base">
-              Навіть 1 гривня може в купі зібрати мільйон. Не обезцінюй свій вклад!
+              Навіть 1 гривня може в купі зібрати мільйон. Не обезцінюй свій
+              вклад!
             </p>
           </div>
         </div>
-        <div className="w-full bg-gradient-to-br from-gr-300 from-0% to-gr-500
-                       to-100% rounded-3xl flex gap-12 border border-accent-100 p-6">
+        <div
+          className="w-full bg-gradient-to-br from-gr-300 from-0% to-gr-500
+                       to-100% rounded-3xl flex gap-12 border border-accent-100 p-6"
+        >
           <div className="w-5/12 flex flex-col justify-center align-center">
             <img width={216} height={216} src={verified} alt="verified" />
           </div>
           <div className="w-7/12 p-4 flex flex-col justify-center mx-auto">
-            <h2 className="text-xl font-bold mb-4">Лише перевірені оголошення</h2>
+            <h2 className="text-xl font-bold mb-4">
+              Лише перевірені оголошення
+            </h2>
             <p className="text-gray-700 text-base">
-              Ми власноруч перевіряємо кожне оголошення на правдивість, тим самим забезпечуємо Вам безпечний донат!
+              Ми власноруч перевіряємо кожне оголошення на правдивість, тим
+              самим забезпечуємо Вам безпечний донат!
             </p>
           </div>
         </div>
       </div>
       <Filters />
       <CardList />
-    </main>
+    </>
   );
 }
 
